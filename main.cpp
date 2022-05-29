6293e4c5218a102f13731ebc9186a2753df456bb
SamusenkoNN <samusenko_nik@inbox.ru>
2022-05-29 16:46:06 +0300

    code: загрузка файла по сети

--- main.cpp
+++ main.cpp
@@ -113,12 +113,26 @@ show_histogram_text(const vector<size_t>& bins)
 
 int main(int argc, char* argv[])
 {
+
+
+
+
     if (argc > 1) {
         for(int i=0; i<argc; i++){
             cout<<"argv["<<i+1<<"]= "<<argv[i]<<endl;
         }
+    CURL *curl = curl_easy_init();
+    if(curl) {
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+    }
+
         return 0;
     }
+
+
 
     curl_global_init(CURL_GLOBAL_ALL);
 
